a16cf204d1566a2b04efea303c4bcaddcd4eb805
Jose Luis <joseluis.sm.117@gmail.com>
2025-01-22 12:14:37 -0600

    add gitignore

--- ConnectMeBackend/dist/src/user/infraestructure/driving-adapters/user.routes.js
+++ ConnectMeBackend/dist/src/user/infraestructure/driving-adapters/user.routes.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.userRoutes = void 0;
 const express_1 = require("express");
+const UserControllers_1 = require("./UserControllers");
 exports.userRoutes = (0, express_1.Router)();
-exports.userRoutes.get("/", (req, res) => {
-    res.send("hola");
-});
+const userControllers = new UserControllers_1.UserControllers();
+exports.userRoutes.get("/", userControllers.initRoutes);
