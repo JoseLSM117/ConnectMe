f7707814a756ad704253adfd1d85eb2e84013244
Jose Luis <joseluis.sm.117@gmail.com>
2025-01-27 01:06:26 -0600

    feat(UserEntity): add userEntity

---
+++ ConnectMeBackend/src/user/domain/entities/user/UserEntity.ts
@@ -0,0 +1,50 @@
+import { EntityRoot } from "../EntityRoot"
+import { UserEmail, UserFirstName, UserLastName, UserPassword, UserProfilePicture } from "./valueObjects"
+
+interface PrimitiveData {
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+  profilePicture?: string
+}
+export class User extends EntityRoot<User, PrimitiveData> {
+  readonly firstName: UserFirstName
+  readonly lastName: UserLastName
+  readonly email: UserEmail
+  readonly password: UserPassword
+  readonly profilePicture?: UserProfilePicture
+
+  constructor({ firstName, lastName, email, password, profilePicture }: { firstName: UserFirstName, lastName: UserLastName, email: UserEmail, password: UserPassword, profilePicture?: UserProfilePicture }) {
+    super()
+    this.firstName = firstName
+    this.lastName = lastName
+    this.email = email
+    this.password = password
+    this.profilePicture = profilePicture
+  }
+
+  static create(firstName: UserFirstName, lastName: UserLastName, email: UserEmail, password: UserPassword, profilePicture?: UserProfilePicture): User {
+    return new User({ firstName, lastName, email, password, profilePicture })
+  }
+
+  static fromPrimitives(plainData: { firstName: string, lastName: string, email: string, password: string, profilePicture?: string }): User {
+    return new User({
+      firstName: new UserFirstName(plainData.firstName),
+      lastName: new UserLastName(plainData.lastName),
+      email: new UserEmail(plainData.email),
+      password: new UserPassword(plainData.password),
+      profilePicture: plainData.profilePicture ? new UserProfilePicture(plainData.profilePicture) : undefined
+    })
+  }
+
+  toPrimitives(): PrimitiveData {
+    return {
+      firstName: this.firstName.value,
+      lastName: this.lastName.value,
+      email: this.email.value,
+      password: this.password.value,
+      profilePicture: this.profilePicture?.value
+    }
+  }
+}
---
+++ ConnectMeBackend/src/user/domain/entities/user/valueObjects.ts
@@ -0,0 +1,46 @@
+import { StringValueObject } from "@src/shared/domain/value-object/StringValueObject"
+import { InvalidEmail, InvalidFirstNameOrLastName, InvalidPassword } from "./Exceptions"
+
+export class UserFirstName extends StringValueObject {
+  constructor(value: string) {
+    super(value)
+    this.ensureValidLength(value)
+  }
+
+  private ensureValidLength(value: string): void {
+    if (value.length < 2 || value.length > 60) {
+      throw new InvalidFirstNameOrLastName()
+    }
+  }
+}
+
+export class UserLastName extends UserFirstName { }
+
+export class UserEmail extends StringValueObject {
+  constructor(value: string) {
+    super(value)
+    this.ensureValidEmail(value)
+  }
+
+  private ensureValidEmail(value: string): void {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (!regex.test(value)) {
+      throw new InvalidEmail()
+    }
+  }
+}
+
+export class UserPassword extends StringValueObject {
+  constructor(value: string) {
+    super(value)
+    this.ensureValidLength(value)
+  }
+
+  private ensureValidLength(value: string): void {
+    if (value.length < 6 || value.length > 60) {
+      throw new InvalidPassword()
+    }
+  }
+}
+
+export class UserProfilePicture extends StringValueObject {}
