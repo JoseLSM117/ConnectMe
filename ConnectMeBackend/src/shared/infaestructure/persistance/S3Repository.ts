aee2ac70b83b9e72ddb4327bd339eff8a54a5e01
Jose Luis <joseluis.sm.117@gmail.com>
2025-01-29 15:21:20 -0600

    feature(envs): fix envs

--- ConnectMeBackend/src/shared/infaestructure/persistance/S3Repository.ts
+++ ConnectMeBackend/src/shared/infaestructure/persistance/S3Repository.ts
@@ -1,15 +1,14 @@
 
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import { envs } from "../env/envs";
 export abstract class S3Repository {
   private readonly _client: S3Client;
 
   constructor() {
     this._client = new S3Client({
-      region: envs.AWS_REGION,
+      region: process.env.AWS_REGION,
       credentials: {
-        accessKeyId: envs.AWS_ACCESS_KEY_ID,
-        secretAccessKey: envs.AWS_SECRET_ACCESS_KEY
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? "",
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? ""
       }
     });
   }
@@ -23,7 +22,7 @@ export abstract class S3Repository {
   async s3UploadFile(uploadData: { file: Buffer, fileName: string }): Promise<void> {
     const client = this.client();
     const params = new PutObjectCommand({
-      Bucket: envs.AWS_BUCKET_NAME ?? "connectme",
+      Bucket: process.env.AWS_BUCKET_NAME ?? "connectme",
       Key: `${this.bucketSubFolders()}/${uploadData.fileName}`,
       Body: uploadData.file
     });
